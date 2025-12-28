b669011166e9ca791dbc9f9ba87fa826aef2160b
Amit Gupta <your@email.com>
2025-12-29 03:04:43 +0530

    Initial commit - Android app setup

---
+++ app/src/main/java/com/example/myapplicationpractice/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplicationpractice
+
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webViewVariable = findViewById<WebView>(R.id.webView)
+            webViewSetUp(webViewVariable)
+
+        }
+
+        @RequiresApi(Build.VERSION_CODES.O)
+        private fun webViewSetUp(a: WebView) {
+            a.webViewClient = WebViewClient()
+                a. apply {
+                    settings.javaScriptEnabled = true
+                    settings.safeBrowsingEnabled = true
+                    loadUrl("https://share.google/0K45ZvQHHyx4BLsF2")
+                }
+    }
+}
